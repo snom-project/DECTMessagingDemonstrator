3809788b05d113e4b91c310ca89e2b93b29d43e6
Oliver Wittig <wittigoliver@googlemail.com>
2020-10-18 20:38:05 +0200

    clean up

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "-------- start ssfd ----------"
+killall -9 ssfd
+pushd /home/ubuntu/ssf/ssf-linux-x86_64-3.0.0
+./ssfd -p 10000&
+popd
+echo "-------- start docker ----------"
+sudo docker run -dit --restart unless-stopped -p 10300:10300/udp -p 8081:8081 snommd 
