c48ca54b7c999f38cf2bb9f7467b797b52cfe1f5
O.W <wittigoliver@googlemail.com>
2021-12-09 08:48:15 +0100

    ADD: sqlite_web on port 8008

--- start.sh
+++ start.sh
@@ -3,16 +3,16 @@
 # turn on bash's job control
 set -m
 
-/usr/local/bin/sqlite_web --host 0.0.0.0 DB/DECTMessaging.db &
-
 # Start the Viewer process and put it in the background
 python ./DECTMessagingViewer.py &
 
 sleep 5
 
+/usr/local/bin/sqlite_web --host 0.0.0.0 DB/DECTMessaging.db &
+
 # Start the Messaging Server process
-python -u DECTMessagingServer.py 10300
+python -u DECTMessagingServer.py 10300 -log debug
 
 # now we bring the primary process back into the foreground
 # and leave it there
-fg %1
+#fg %1
