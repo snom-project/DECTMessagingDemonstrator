96a3dbea45a87dd200ecab1a00243b1ad3a40eec
O.W <wittigoliver@googlemail.com>
2023-01-25 15:38:22 +0100

    mqtt missing

---
+++ app/mqtt/mqttasync.py
@@ -0,0 +1,102 @@
+import asyncio
+from contextlib import AsyncExitStack, asynccontextmanager
+from random import randrange
+from asyncio_mqtt import Client, MqttError
+import time
+
+
+async def advanced_example():
+    # We 💛 context managers. Let's create a stack to help
+    # us manage them.
+    async with AsyncExitStack() as stack:
+        # Keep track of the asyncio tasks that we create, so that
+        # we can cancel them on exit
+        tasks = set()
+        stack.push_async_callback(cancel_tasks, tasks)
+
+        # Connect to the MQTT broker
+        client = Client("192.168.188.201")
+        await stack.enter_async_context(client)
+
+        # You can create any number of topic filters
+        topic_filters = (
+            "floors/+/humidity",
+            "floors/rooftop/#"
+            # 👉 Try to add more filters!
+        )
+        for topic_filter in topic_filters:
+            # Log all messages that matches the filter
+            manager = client.filtered_messages(topic_filter)
+            messages = await stack.enter_async_context(manager)
+            template = f'[topic_filter="{topic_filter}"] {{}}'
+            task = asyncio.create_task(log_messages(messages, template))
+            tasks.add(task)
+
+        # Messages that doesn't match a filter will get logged here
+        messages = await stack.enter_async_context(client.unfiltered_messages())
+        task = asyncio.create_task(log_messages(messages, "[unfiltered] {}"))
+        tasks.add(task)
+
+        # Subscribe to topic(s)
+        # 🤔 Note that we subscribe *after* starting the message
+        # loggers. Otherwise, we may miss retained messages.
+        await client.subscribe("floors/#")
+
+        # Publish a random value to each of these topics
+        topics = (
+            "floors/basement/humidity",
+            "floors/rooftop/humidity",
+            "floors/rooftop/illuminance",
+            # 👉 Try to add more topics!
+        )
+        task = asyncio.create_task(post_to_topics(client, topics))
+        tasks.add(task)
+
+        # Wait for everything to complete (or fail due to, e.g., network
+        # errors)
+        await asyncio.gather(*tasks)
+
+async def post_to_topics(client, topics):
+    while True:
+        for topic in topics:
+            message = randrange(100)
+            print(f'[topic="{topic}"] Publishing message={message}')
+            await client.publish(topic, message, qos=1)
+            await asyncio.sleep(2)
+
+async def log_messages(messages, template):
+    async for message in messages:
+        # 🤔 Note that we assume that the message paylod is an
+        # UTF8-encoded string (hence the `bytes.decode` call).
+        print(template.format(message.payload.decode()))
+
+async def cancel_tasks(tasks):
+    for task in tasks:
+        if task.done():
+            continue
+        try:
+            task.cancel()
+            await task
+        except asyncio.CancelledError:
+            pass
+
+async def main():
+    # Run the advanced_example indefinitely. Reconnect automatically
+    # if the connection is lost.
+    reconnect_interval = 3  # [seconds]
+    while True:
+        try:
+            await advanced_example()
+        except MqttError as error:
+            print(f'Error "{error}". Reconnecting in {reconnect_interval} seconds.')
+        finally:
+            await asyncio.sleep(reconnect_interval)
+
+if __name__ == "__main__":
+    i = 0
+    while True:
+        print(i)
+        i=i+1
+        asyncio.run(main())
+        time.sleep(1.0)
+
